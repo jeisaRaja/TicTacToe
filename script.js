87c326ea2cf204758c0b0e17e99a2bb801e218a5
jeisaraja <jeisa.raja.jrt@gmail.com>
2021-12-15 12:07:18 +0700

    Added gameboard object and css styling

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const body = document.querySelector('body');
-const board = body.querySelector('.board')
-let array = ["","","","","","","","",""];
+const board = body.querySelector('.board');
 
-for(let x=0 ; x<9 ; x++){
-    let array_div = document.createElement('div');
-    array_div.innerHTML = "Hey";
-    array_div.setAttribute('class', 'board_item')
-    board.appendChild(array_div);
-}
+
+const gameboard = (()=>{
+    let array = ["","","","","","","","",""];
+    for(let x of array){
+        console.log(x)
+        let array_div = document.createElement('div');
+        array_div.innerHTML = "Hey";
+        array_div.setAttribute('class', 'board_item');
+        board.appendChild(array_div);
+    }
+
+    const setBox = (index,sign)=>{
+        array[index] = sign;
+    }
+    const getBox = ()=>{
+        alert('hello!')
+    }
+
+    return {setBox, getBox, array};
+
+})();
+
+
+
+
+
+
