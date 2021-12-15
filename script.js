edbcaaeb3d534d5347fbc51d7358102c5947ce59
jeisaraja <jeisa.raja.jrt@gmail.com>
2021-12-15 20:17:46 +0700

    Added eventlistener to gameboard

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const board = body.querySelector('.board');
 
 const gameboard = (()=>{
     let array = ["","","","","","","","",""];
-    for(let x of array){
-        console.log(x)
+    for(let i=0 ; i<9 ; i++){
         let array_div = document.createElement('div');
-        array_div.innerHTML = "Hey";
+        array_div.id =i;
+        array_div.innerHTML = "";
         array_div.setAttribute('class', 'board_item');
         board.appendChild(array_div);
+
     }
 
     const setBox = (index,sign)=>{
@@ -23,8 +24,21 @@ const gameboard = (()=>{
 
 })();
 
-
-
+const Player = (name,role)=>{
+    function isWinning(){
+        alert("Not yet!")
+    }
+    return {name,role,isWinning}
+}
+const array_divs = document.querySelectorAll('.board_item')
+array_divs.forEach(div=>div.addEventListener('click',(e)=>{
+    console.log(e.path)
+}))
+function plays(role){
+    const index = 1;
+    gameboard.setBox(index,role)
+}
+console.log(array_divs)
 
 
 
